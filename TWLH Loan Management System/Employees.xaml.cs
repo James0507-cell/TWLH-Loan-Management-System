3b2d6860de0acb34a82556e1fc3ec09d5ee81cbf
Lester Arigo <146509198+JohnLesterArigo@users.noreply.github.com>
2026-02-27 17:50:11 +0800

    employee client added

---
+++ TWLH Loan Management System/Employees.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Windows.Controls;
+
+namespace TWLH_Loan_Management_System
+{
+    public partial class Employees : Page 
+    {
+       
+        dbManager db = new dbManager();
+
+        public Employees()
+        {
+           
+            InitializeComponent();
+            LoadEmployeeData();
+        }
+
+        private void LoadEmployeeData()
+        {
+            string query = "SELECT CONCAT(first_name, ' ', last_name) AS Name, role AS Role, email AS Email, is_active AS Status, created_at AS DateHired FROM tbl_employee";
+
+            DataTable dt = db.displayRecords(query);
+            if (dt != null)
+            {
+                EmployeeListView.ItemsSource = dt.DefaultView;
+            }
+        }
+    }
+}
--- TWLH Loan Management System/MainWindow.xaml.cs
+++ TWLH Loan Management System/MainWindow.xaml.cs
@@ -104,6 +104,7 @@ namespace TWLH_Loan_Management_System
         private void btnEmployee_Click(object sender, RoutedEventArgs e)
         {
             SetActiveButton(sender as Button);
+            MainFrame.Navigate(new Employees());
         }
         private void btnFollowUp_Click(object sender, RoutedEventArgs e)
         {
